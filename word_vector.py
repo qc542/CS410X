1cbe5417beab989437081462d5b5ad486af1381c
qc542 <36024574+qc542@users.noreply.github.com>
2020-04-03 14:26:56 -0400

    Made minor fixes to formatting

--- word_vector.py
+++ word_vector.py
@@ -1,6 +1,8 @@
 from typing import List
 
 Vector = List[float]
+
+
 # A type alias in Python 3; makes the interpreter treat Vector and List[float] as interchangeable equivalents
 
 class Word:
@@ -9,6 +11,7 @@ class Word:
         self.text = text
         self.vector = vector
 
+
 def vector_len(v: Vector) -> float:
     # Python 3 optionally allows declaring the parameter type by inserting
     # the type after a colon
@@ -16,29 +19,32 @@ def vector_len(v: Vector) -> float:
     # The length of a vector is the square root of the sum of
     # each of its component squared
     # Function returns a floating point number
-    return math.sqrt(sum([x*x for x in v]))
+    return math.sqrt(sum([x * x for x in v]))
+
 
 def dot_product(v1: Vector, v2: Vector) -> float:
     assert len(v1) == len(v2), "Lengths of vectors are unequal."
     # The assert statement evaluates the succeeding expression and 
     # raises an AssertionError exception when the assertion fails
     # The specified message will be printed in case of a failure
-    
+
     # len(v1) does not invoke the vector_len function defined above
     # It simply returns the number of components in v1
 
     # The dot product equals the sum of each component in v1 multiplied
     # by the corresponding component in v2 at the same index
-    return sum([x*y for (x,y) in zip(v1,v2)]) 
+    return sum([x * y for (x, y) in zip(v1, v2)])
+
 
 def cosine_similarity(v1: Vector, v2: Vector) -> float:
     # The cosine of the angle between v1 and v2 equals their dot product
     # divided by the product of their lengths (as in Euclidean norm)
 
-    return dot_product(v1,v2) / (vector_len(v1) * vector(v2))
+    return dot_product(v1, v2) / (vector_len(v1) * vector(v2))
+
 
 def sorted_by_similarity(words: List[Word], base_vector: Vector) \
--> List[tuple]:
+        -> List[tuple]:
     """ List[Word] refers to the Word class defined above, which contains the word vector's components represented as a list of floating point numbers,
     along with the word itself.
     base_vector is simply a given word represented as floating point numbers, which the function compares each target word against to yield a ranking
@@ -56,4 +62,4 @@ def sorted_by_similarity(words: List[Word], base_vector: Vector) \
     t[0] indicates the lambda function returns the first element in each tuple in the list, which becomes the key for sorting.
     Enabling the reverse option makes the list sorted in descending order, which matches the order of most to least similar. """
 
-    return sorted(tuple_list, key=lambda t:t[0], reverse=True)
+    return sorted(tuple_list, key=lambda t: t[0], reverse=True)
